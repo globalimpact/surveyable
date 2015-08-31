bf91175cd82bee3f80aa8d880fc56dac0040136b
Lee Dykes <actpic07@yahoo.com>
2015-08-31 14:58:08 -0500

    loading ActsAsSurvey from main module file

--- lib/surveyable.rb
+++ lib/surveyable.rb
@@ -10,3 +10,7 @@ require "surveyable/acts_as_response"
 require "surveyable/answer"
 require "surveyable/answer_choice"
 require 'surveyable/question'
+
+ActiveSupport.on_load(:active_record) do
+  include Surveyable::ActsAsSurvey
+end
