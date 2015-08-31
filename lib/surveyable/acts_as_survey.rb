e0483543337f7826e48d593550f2586f27946513
Lee Dykes <actpic07@yahoo.com>
2015-08-31 14:30:58 -0500

    Moving away from concerns setup to model setup

--- lib/surveyable/acts_as_survey.rb
+++ lib/surveyable/acts_as_survey.rb
@@ -1,17 +1,14 @@
 module Surveyable
   module ActsAsSurvey
-    extend ActiveSupport::Concern
-    included do
-      has_many :questions, as: :survey, class_name: 'Surveyable::Question'
-
+    def self.included(base)
+      base.send :extend, ClassMethods
     end
     module ClassMethods
       def acts_as_survey(response_relationship, options = {})
+        has_many :questions, as: :survey, class_name: 'Surveyable::Question'
         #cattr_accessor :responses
         #self.responses = responses
       end
     end
   end
 end
-
-ActiveRecord::Base.extend(Surveyable::ActsAsSurvey)
