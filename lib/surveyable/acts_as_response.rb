c4acbd42295ba9e815dade02b7a896556a0b34c9
Lee Dykes <actpic07@yahoo.com>
2015-09-01 08:14:49 -0500

    allowing response to create/edit answers

--- lib/surveyable/acts_as_response.rb
+++ lib/surveyable/acts_as_response.rb
@@ -7,7 +7,7 @@ module Surveyable
     module ClassMethods
       def acts_as_response(options = {})
         has_many :answers, as: :response, class_name: 'Surveyable::Answer'
-
+        accepts_nested_attributes_for :answers
         send :include, InstanceMethods
       end
     end
